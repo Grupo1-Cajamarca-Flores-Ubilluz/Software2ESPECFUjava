06933294eb426d7737255a8af5ce77a250b87a7a
Andrez-Flores <marlon-flores-87@hotmail.com>
2017-11-08 14:57:09 -0600

    sgbstdn lectura

--- src/com/ecu/Software2CFU.java
+++ src/com/ecu/Software2CFU.java
@@ -17,6 +17,7 @@ public class Software2CFU {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola version 1");
+        System.out.println("Commit");
     }
     
 }
