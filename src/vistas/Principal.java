e08f43b9f0d37cd9471064cfff0bea99070fd836
joct-16 <33468656+joct-16@users.noreply.github.com>
2017-11-13 15:29:34 -0500

    Actualizacion codigo
    Actualizacion codigo fuente clase principal

--- src/vistas/Principal.java
+++ src/vistas/Principal.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package vistas;
+package Vistas;
 
 import javax.swing.JOptionPane;
 
@@ -151,7 +151,6 @@ public class Principal extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
