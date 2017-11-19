d6940de29379244acf7f7e8d71949cf89b28bb61
joct-16 <33468656+joct-16@users.noreply.github.com>
2017-11-19 12:45:16 -0500

    Actualización de clase
    Se actualiza la clase ModeloVenta añadiendo la funcionalidad faltante

--- src/com/ecu/ModeloVenta.java
+++ src/com/ecu/ModeloVenta.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.ecu;
+package almacenes_paul;
 
 import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
@@ -30,69 +30,57 @@ public class ModeloVenta {
         con = new Conexion();
     }
     
-    public void ingresarUsuario(Cliente c){
-        query="insert into CLIENTE (cedula, nombres, apellidos, telfFijo, telfMovil, direccion, correo)VALUES (?,?,?,?,?,?,?);";
+    public boolean ingresarVenta(Venta v){
+        query="insert into VENTA (idCliente, total, fecha)VALUES (?,?,?);";
         
         try{
             pst=con.getConnection().prepareStatement(query);
-            pst.setString(1, c.getCedula());
-            pst.setString(2, c.getNombres());
-            pst.setString(3, c.getApellidos());
-            pst.setString(4, c.getTelfFijo());
-            pst.setString(5, c.getTelfMovil());
-            pst.setString(6, c.getDireccion());
-            pst.setString(7, c.getCorreo());
+            pst.setInt(1, v.getIdCliente());
+            pst.setFloat(2, v.getTotal());
+            pst.setString(3, v.getFecha().getAa()+"-"+v.getFecha().getMm()+"-"+v.getFecha().getDd());
+            
             pst.executeUpdate();
-            JOptionPane.showMessageDialog(null,"Usuario ingresado correctamente");
+            return true;
+        }catch(SQLException ex1)
+        {
+            System.out.println(""+ex1.getMessage());
+            return false;
+        }
+    }
+    
+   public void ingresarDetalle(int codP, int codV, int cant){
+        query="insert into productos_venta values(?,?,?);";
+        
+        try{
+            pst=con.getConnection().prepareStatement(query);
+            pst.setInt(1, codV);
+            pst.setInt(2, codP);
+            pst.setInt(3, cant);
+            pst.executeUpdate();
+            //JOptionPane.showMessageDialog(null,"detalle ingresado correctamente");
         }catch(SQLException ex1)
         {
             System.out.println(""+ex1.getMessage());
         }
     }
     
-    /*
-    public void actualizarRegistro(String nombre, String apellido, int edad, String correo, String numCuenta){
-     
-        try {
-            int cod= Integer.parseInt(JOptionPane.showInputDialog("Ingrese el codigo del usuario"));
-            query="update usuario set "+"nombre='"+nombre+"', apellido='"+apellido+"', edad="+edad+", correo='"+correo+"', numCuenta='"+numCuenta+
-                "' where id="+cod+";";
-            pst = con.getConnection().prepareStatement(query);
-            pst.executeUpdate();
-        } catch (SQLException ex) {
-            System.out.println(""+ex.getMessage());
-        }
-    }
-    
-    public void eliminarRegistro(){
-        try {
-            int cod= Integer.parseInt(JOptionPane.showInputDialog("Ingrese el codigo del usuario"));
-            query="delete from usuario where id="+cod+";";
-            pst = con.getConnection().prepareStatement(query);
-            pst.executeUpdate();
-        } catch (SQLException ex) {
-            System.out.println(""+ex.getMessage());
-        }
-    }*/
-    
-    public ArrayList<Cliente> mostrarDatos(){
-        ArrayList<Cliente> datos = new ArrayList<>();
-        query="select * from cliente";
+   public ArrayList<Venta> mostrarDatos(){
+        ArrayList<Venta> datos = new ArrayList<>();
+        query="select v.idVenta, c.cedula, c.nombres, c.apellidos, v.total, v.fecha " +
+                "from cliente c join venta v " +
+                "on (v.idCliente = c.idCliente);";
         
         try{
             st=con.getConnection().createStatement();
             rs=st.executeQuery(query);
             while(rs.next()){
-                Cliente c = new Cliente();
-                c.setCod(rs.getInt(1));
-                c.setCedula(rs.getString(2));
-                c.setNombres(rs.getString(3));
-                c.setApellidos(rs.getString(4));
-                c.setTelfFijo(rs.getString(5));
-                c.setTelfMovil(rs.getString(6));
-                c.setDireccion(rs.getString(7));
-                c.setCorreo(rs.getString(8));
-                datos.add(c);
+                Venta v = new Venta();
+                v.setId(rs.getInt(1));
+                v.setAux(rs.getString(2)+" "+rs.getString(3)+" "+rs.getString(4));
+                v.setTotal(Float.parseFloat(rs.getString(5)));
+                v.setF(rs.getString(6));
+                
+                datos.add(v);
             }
             
         }catch(SQLException ex1)
@@ -101,6 +89,25 @@ public class ModeloVenta {
         }
         
         return datos;
+    }
+   
+    public int totalVentas(){
+        ArrayList<Venta> datos = new ArrayList<>();
+        query="select * from venta";
+        int i=0;
+        try{
+            st=con.getConnection().createStatement();
+            rs=st.executeQuery(query);
+            while(rs.next()){
+                i++;
+            }
+            
+        }catch(SQLException ex1)
+        {
+            System.out.println(""+ex1.getMessage());
+        }
+        
+        return i;
     }
     
 }
