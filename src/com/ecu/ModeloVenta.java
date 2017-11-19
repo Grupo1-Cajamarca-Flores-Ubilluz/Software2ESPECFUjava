cb0d806671167a7e2c7801f467a8a2500fb70cde
joct-16 <33468656+joct-16@users.noreply.github.com>
2017-11-19 12:47:22 -0500

    Actualización
    Actualización package

--- src/com/ecu/ModeloVenta.java
+++ src/com/ecu/ModeloVenta.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package almacenes_paul;
+package com.ecu;
 
 import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
